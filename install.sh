4e3bb95a088faa60e74989838d376f84d55f62c1
Malcolm Fell <emarref@gmail.com>
2015-08-31 10:13:57 +1200

    Overwrite

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-[ -z "$DEST_DIR" ] && DEST_DIR=`pwd`
-[ -z "$CONFIG_DIR" ] && CONFIG_DIR="${DEST_DIR}/app/config/vm"
+DEST_DIR=`pwd`
+CONFIG_DIR="${DEST_DIR}/app/config/vm"
 
 curl -o /tmp/symfony.zip -L https://github.com/emarref/vagrant-project/archive/symfony.zip
 
-unzip -j /tmp/symfony.zip "vagrant-project-symfony/Vagrantfile" -d "$DEST_DIR"
+unzip -o -j /tmp/symfony.zip "vagrant-project-symfony/Vagrantfile" -d "$DEST_DIR"
 
 [ ! -d "$CONFIG_DIR" ] && mkdir -p "$CONFIG_DIR"
-unzip -j /tmp/symfony.zip "vagrant-project-symfony/app/config/vm/*" -d "$CONFIG_DIR"
+unzip -o -j /tmp/symfony.zip "vagrant-project-symfony/app/config/vm/*" -d "$CONFIG_DIR"
